256195f7ee0d36358922b4b6770bac51b4d960cb
Zheng Ma <mazhengcn@outlook.com>
2022-09-05 11:22:33 +0000

    refactor(nvidia-cuda): update nvidia-cuda install.sh

--- src/nvidia-cuda/install.sh
+++ src/nvidia-cuda/install.sh
@@ -1,21 +1,11 @@
 #!/usr/bin/env bash
 
-set -e
+INSTALL_CUDNN=${INSTALLCUDNN:-"true"}
+INSTALL_NVTX=${INSTALLNVTX:-"true"}
+INSTALL_NVCC=${INSTALLNVCC:-"true"}
+CUDA_VERSION=${CUDAVERSION:-"11.7"}
+CUDNN_VERSION=${CUDNNVERSION:-"8.5.0.96"}
 
-INSTALL_CUDNN=${INSTALLCUDNN}
-INSTALL_NVTX=${INSTALLNVTX}
-INSTALL_NVCC=${INSTALLNVCC}
-CUDA_VERSION=${CUDAVERSION}
-CUDNN_VERSION=${CUDNNVERSION}
-
-if [ "$(id -u)" -ne 0 ]; then
-    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
-    exit 1
-fi
-
-# Install dependencies
-apt-get update -yq
-apt-get install -yq wget ca-certificates
 
 # Add NVIDIA's package repository to apt so that we can download packages
 # Always use the ubuntu2004 repo because the other repos (e.g., debian11) are missing packages
@@ -24,42 +14,81 @@ KEYRING_PACKAGE="cuda-keyring_1.0-1_all.deb"
 KEYRING_PACKAGE_URL="$NVIDIA_REPO_URL/$KEYRING_PACKAGE"
 KEYRING_PACKAGE_PATH="$(mktemp -d)"
 KEYRING_PACKAGE_FILE="$KEYRING_PACKAGE_PATH/$KEYRING_PACKAGE"
-wget -O "$KEYRING_PACKAGE_FILE" "$KEYRING_PACKAGE_URL"
-apt-get install -yq "$KEYRING_PACKAGE_FILE"
-apt-get update -yq
 
 # Ensure that the requested version of CUDA is available
-cuda_pkg="cuda-libraries-${CUDA_VERSION/./-}"
-nvtx_pkg="cuda-nvtx-${CUDA_VERSION/./-}"
-nvcc_pkg="cuda-nvcc-${CUDA_VERSION/./-}"
-if ! apt-cache show "$cuda_pkg"; then
-    echo "The requested version of CUDA is not available: CUDA $CUDA_VERSION"
-    exit 1
-fi
-
+CUDA_PKG="cuda-libraries-${CUDA_VERSION/./-}"
+NVTX_PKG="cuda-nvtx-${CUDA_VERSION/./-}"
+NVCC_PKG="cuda-nvcc-${CUDA_VERSION/./-}"
 # Ensure that the requested version of cuDNN is available AND compatible
-cudnn_pkg_version="libcudnn8=${CUDNN_VERSION}-1+cuda${CUDA_VERSION}"
-if ! apt-cache show "$cudnn_pkg_version"; then
-    echo "The requested version of cuDNN is not available: cuDNN $CUDNN_VERSION for CUDA $CUDA_VERSION"
+CUDNN_PKG_VERSION="libcudnn8=${CUDNN_VERSION}-1+cuda${CUDA_VERSION}"
+
+set -eux
+export DEBIAN_FRONTEND=noninteractive
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
     exit 1
 fi
 
-echo "Installing CUDA libraries..."
-apt-get install -yq "$cuda_pkg"
+# Ensure that login shells get the correct path if the user updated the PATH using ENV.
+rm -f /etc/profile.d/00-restore-env.sh
+echo "export PATH=${PATH//$(sh -lc 'echo $PATH')/\$PATH}" > /etc/profile.d/00-restore-env.sh
+chmod +x /etc/profile.d/00-restore-env.sh
 
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt-get update -y
+        apt-get -y install --no-install-recommends "$@"
+
+        # Clean up
+        apt-get clean -y
+        rm -rf /var/lib/apt/lists/*
+    fi
+}
+
+check_version() {
+    if ! apt-cache show "$1"; then
+        echo "The requested version of $1 is not available!"
+        exit 1
+    fi
+}
+
+install_packages() {
+    echo "(*) Installing "$@"..."
+    apt-get -y install --no-install-recommends "$@"
+
+    # Clean up
+    apt-get clean -y
+    rm -rf /var/lib/apt/lists/*
+}
+
+# Install dependencies
+echo "Installing dependencies..."
+check_packages wget ca-certificates
+
+wget -O "$KEYRING_PACKAGE_FILE" "$KEYRING_PACKAGE_URL"
+apt-get -y install --no-install-recommends "$KEYRING_PACKAGE_FILE"
+apt-get update -y
+
+check_version "$CUDA_PKG"
+
+package_list=("$CUDA_PKG")
 if [ "$INSTALL_CUDNN" = "true" ]; then
-    echo "Installing cuDNN libraries..."
-    apt-get install -yq "$cudnn_pkg_version"
+    check_version $CUDNN_PKG_VERSION
+    package_list+=("$CUDNN_PKG_VERSION")
 fi
 
 if [ "$INSTALL_NVTX" = "true" ]; then
-    echo "Installing NVTX..."
-    apt-get install -yq "$nvtx_pkg"
+    check_version $NVTX_PKG
+    package_list+=("$NVTX_PKG")
 fi
 
 if [ "$INSTALL_NVCC" = "true" ]; then
-    echo "Installing NVCC..."
-    apt-get install -yq "$nvcc_pkg"
+    check_version $NVCC_PKG
+    package_list+=("$NVCC_PKG")
 fi
+
+install_packages $package_list
 
 echo "Done!"
